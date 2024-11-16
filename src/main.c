c77de81c081f91cdab9b3871c47295b3f7286b12
Khaoula Chetioui <khaoulachetioui4@gmail.com>
2024-11-16 03:35:58 +0100

    add read and interactif mode

--- src/main.c
+++ src/main.c
@@ -9,6 +9,8 @@
 #include "types.h"
 #include "eval.h"
 
+
+
 void free_ast(ASTNode* node) {
     if (!node) return;
     free_ast(node->left);
@@ -38,22 +40,62 @@ void interpret(const char* input) {
         free_ast(root);
     }
 }
+void read_file(const char *f){
+    FILE *file = fopen(f,"r");
+    if(file ==NULL){
+        printf("Erreur: Impossible d ouvrir le fichier %s \n ", f);
+        return;
 
-// Fonction principale
+    }
+
+    char line[256]; //taille max pour une line 
+
+    while(fgets(line, sizeof(line), file)){
+        // supprimer le car de fin de ligne s il existe 
+        int len = strlen(line);
+        if (len> 0 && line[len-1] == '\n'){
+            line[ len -1 ] = '\0';
+        }
+      //  printf("Insctruction: %s\n",line);
+        interpret(line);
+    }
+    fclose(file); // Ferme le fichier
+    printf("Fin de la lecture du fichier %s.\n", f);
+}
+// Fonction principale avec REPL
 int main() {
-    printf("Bienvenue dans l\'interpreteur. Tapez 'exit' pour quitter.\n");
+    printf("Bienvenue dans l'interpréteur\n");
+    printf("Tapez 'exit' pour quitter ou 'file:<nom_du_fichier>' pour exécuter un fichier.\n");
 
-    char input[100];
+    char input[256];  // entrées utilisateur
+
     while (1) {
-        printf("> ");
-        fgets(input, sizeof(input), stdin);
-        if (strncmp(input,"exit", 4)== 0) {
-            printf("Au revoir!\n");
+        printf("> ");  // Invite de commande
+        if (!fgets(input, sizeof(input), stdin)) {
+            break;  // Arrête la boucle en cas d'erreur de lecture
+        }
 
-        
+        // Supprimer le caractère de fin de ligne s'il existe
+        size_t len = strlen(input);
+        if (len > 0 && input[len - 1] == '\n') {
+            input[len - 1] = '\0';
+        }
+
+        // Vérifier si l'utilisateur veut quitter
+        if (strcmp(input, "exit") == 0) {
+            printf("Au revoir !\n");
             break;
         }
-        interpret(input);
+
+
+        // Vérifier si l'utilisateur demande un fichier
+        if (strncmp(input, "file:", 5) == 0) {
+            char *file = input + 5;  // Extraire le nom du fichier
+            read_file(file);  // Appeler une fonction existante pour lire un fichier
+        } else {
+            // Interpréter l'entrée utilisateur
+            interpret(input);
+        }
     }
 
     return 0;
